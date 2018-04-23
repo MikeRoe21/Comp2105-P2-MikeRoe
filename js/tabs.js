8b46d0b2b3cd8a4cb7c68ec73c75d446da06e887
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2018-04-23 01:30:01 -0400

    initial

---
+++ js/tabs.js
@@ -0,0 +1,36 @@
+// Tab Panels - Starter functonality
+$(document).ready(function(){
+	// grab all container panel DIVs
+	var tabContainers = $('div.tabs > div');
+	// hide them all, then show the first one
+	tabContainers.hide().filter(':first').show();
+	// set the first tab to class="selected"
+	$('div.tabs ul.tabNavigation a:first').addClass('selected');
+	// add onclick handlers to all tabs
+	$('div.tabs ul.tabNavigation a').click(function(){
+		// hide all tabContainers
+		tabContainers.hide();
+		// show only the tabContainer with the ID that matches the href for the A that was clicked
+		//alert(this.hash);
+		tabContainers.filter(this.hash).show();
+		// remove class="selected" from all tabs
+		$('div.tabs ul.tabNavigation a').removeClass('selected');
+		// set class="selected" on the tab that was just clicked
+		$(this).addClass('selected');
+		// stop following links
+		return false;
+	});
+});
+
+// GreenSock Animation:
+
+// Using the TweenMax CDN to animate elements on the page, in this case, any 'p' tags
+TweenMax.staggerFrom('p', 1.5, {opacity: 0, scale: .5}, 1);
+// Background Colour set to red before fading
+TweenMax.staggerFrom('p', 5, {backgroundColor:"red"}, 2);
+// Changes font style to italic for 6 seconds
+TweenMax.staggerFrom('p', 6, {fontStyle:"italic"}, 6);
+
+// Using GreenSock, create a large delay for the third tab to display HTML content.
+const duration = 5000;
+const third = document.querySelector('#gsap #third');
